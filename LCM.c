2856686225f0e758772f53c38fd590f44ddfbbb8
sureshreddy777 <119723601+sureshreddy777@users.noreply.github.com>
2023-05-23 20:39:24 +0530

    #include <stdio.h> int main() {     int n1, n2, max;     scanf("%d %d", &n1, &n2);     max = (n1 > n2) ? n1 : n2;     while (1)     {         if ((max % n1 == 0) && (max % n2 == 0)) {             printf("%d", max);             break;         }         ++max;     } }

--- LCM.c
+++ LCM.c
@@ -1,12 +1,15 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+int main() 
 {
-    int i,a,b;
-    scanf("%d%d",&a,&b);
-    for(i=1;i<=a*b;i++)
+    int n1, n2, max;
+    scanf("%d %d", &n1, &n2);
+    max = (n1 > n2) ? n1 : n2;
+    while (1) 
     {
-        if(i%a==0 && i%b==0)
-        break;
+        if ((max % n1 == 0) && (max % n2 == 0)) {
+            printf("%d", max);
+            break;
+        }
+        ++max;
     }
-    printf("%d",i);
 }
